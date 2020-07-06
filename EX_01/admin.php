73dcc7caf88a145cd124ab4ef96f322fc4722039
Louise THIRIET <67266167+louise-thiriet@users.noreply.github.com>
2020-07-06 18:42:42 +0200

    Add files via upload

---
+++ EX_01/admin.php
@@ -0,0 +1,6 @@
+<p>
+    <form method="POST" enctype="multipart/form-data">
+    <input type="file" name="photo" id="Image">
+    <p><input type="submit" name="envoyer" value="Envoyer la photo"></p>
+
+</form>
