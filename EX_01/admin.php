dc94f1163a5516cdebf321ad956688437bc4885e
Louise THIRIET <67266167+louise-thiriet@users.noreply.github.com>
2020-07-07 15:49:52 +0200

    Delete admin.php

--- EX_01/admin.php
+++
@@ -1,6 +0,0 @@
-<p>
-    <form method="POST" enctype="multipart/form-data">
-    <input type="file" name="photo" id="Image">
-    <p><input type="submit" name="envoyer" value="Envoyer la photo"></p>
-
-</form>
